48ae3af76c8bbc8cba763548e7c7f3275e7e84c9
shine <shine.samuel@insysme.com>
2019-11-13 09:46:33 +0400

    changed api url and modified path for read folder.

--- file_upload/src/main/java/com/ins/FileUploadApplication.java
+++ file_upload/src/main/java/com/ins/FileUploadApplication.java
@@ -3,6 +3,7 @@ package com.ins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.ins.property.FileStorageProperties;
